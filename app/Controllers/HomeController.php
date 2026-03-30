8ddf734950138d606ffbe06d709269bd8ff53fcf
Swati Joshi <swatijoshi@outlook.de>
2026-03-31 00:01:54 +0200

    added db image and pdo mysql driver

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -11,14 +11,4 @@ class HomeController
         return View::make('index');
     }
 
-    public function upload()
-    {
-        $filePath = STORAGE_PATH .'/'. $_FILES["file"]["name"];
-        move_uploaded_file($_FILES["file"]["tmp_name"], $filePath);
-
-        echo "<pre>";
-        var_dump(pathinfo($filePath));
-        echo "</pre>";
-
-    }
 }
